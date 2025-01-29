297b15462081b3b42227148c59309c5bd0a7e9af
GVAravindDSB <gvaravinddsb@gmail.com>
2025-01-29 14:11:04 +0530

    Remove deprecated files and scripts from the backend and frontend

--- run.ps1
+++
@@ -1,7 +0,0 @@
-# Start the backend server in a new PowerShell window
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd backend; python main.py"
-
-# Start the frontend app in a new PowerShell window
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd frontend; npm run dev"
-
-# Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-Command", "cd frontend; npm run serverstart"
